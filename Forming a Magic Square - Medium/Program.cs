ad5d904cf2ca4e6c7303a9d34e766245604b9c66
MateuVieira <mateus12v@hotmail.com>
2018-10-31 10:11:05 -0300

    Forming a Magic Square - Problem - Program

---
+++ Forming a Magic Square - Medium/Program.cs
@@ -0,0 +1,28 @@
+﻿static int formingMagicSquare(int[][] s) {
+
+        var square = new List<int>();
+
+        for (int i = 0; i < 3; i++)
+        {
+             foreach (var item in s[i])
+             square.Add(Convert.ToInt32(item));
+        }
+
+        var allMagics = new int[][] 
+        {new int[] { 8, 1, 6, 3, 5, 7, 4, 9, 2},
+         new int[] { 6, 1, 8, 7, 5, 3, 2, 9, 4},
+         new int[] { 4, 9, 2, 3, 5, 7, 8, 1, 6},
+         new int[] { 2, 9, 4, 7, 5, 3, 6, 1, 8},
+         new int[] { 8, 3, 4, 1, 5, 9, 6, 7, 2},
+         new int[] { 4, 3, 8, 9, 5, 1, 2, 7, 6},
+         new int[] { 6, 7, 2, 1, 5, 9, 8, 3, 4},
+         new int[] { 2, 7, 6, 9, 5, 1, 4, 3, 8}};
+
+         var sums = from item in allMagics
+                    select item.Zip(square, (a, b) => Math.Abs(a - b)).Sum();
+
+         //Console.WriteLine(sums.Min());
+
+         return sums.Min();
+
+    }
