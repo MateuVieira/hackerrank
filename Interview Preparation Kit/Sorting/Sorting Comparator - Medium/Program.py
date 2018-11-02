24e4992aa8fb12e6ca65555ed3cee60c6ad525bc
MateuVieira <mateus12v@hotmail.com>
2018-11-02 18:28:14 -0300

    Sorting Comparator - Problem - Program

---
+++ Interview Preparation Kit/Sorting/Sorting Comparator - Medium/Program.py
@@ -0,0 +1,10 @@
+ def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def __repr__(self):
+        pass
+    def comparator(a, b):
+        val = b.score - a.score
+        if val == 0:
+            return -1 if a.name < b.name else 1
+        return val
