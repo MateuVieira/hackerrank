a9a480b99059d27819ec33b4a38161a251e54446
MateuVieira <mateus12v@hotmail.com>
2018-11-03 16:51:52 -0300

    Nested Lists - Problem - Program

---
+++ Python/Nested Lists - Easy/Program.py
@@ -0,0 +1,8 @@
+﻿if __name__ == '__main__':
+    n = int(input())
+    arr = [[input(), float(input())] for _ in range(n)]
+
+    c = sorted(set([b for a,b in arr]))[1]
+
+    print ('\n'.join(sorted([a for a,b in arr if b == c])))
+
