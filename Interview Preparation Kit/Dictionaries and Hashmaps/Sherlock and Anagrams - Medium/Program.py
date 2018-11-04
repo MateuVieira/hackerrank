ccfd8e21e32b0fb726dc72bf35fe770c49aac8c5
MateuVieira <mateus12v@hotmail.com>
2018-11-04 01:37:07 -0200

    Sherlock and Anagrams - Problem - Program

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Sherlock and Anagrams - Medium/Program.py
@@ -0,0 +1,34 @@
+﻿#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sherlockAndAnagrams function below.
+def sherlockAndAnagrams(s):
+    buckets = {}
+    for i in range(len(s)):
+        for j in range(1, len(s) - i + 1):
+            key = frozenset(Counter(s[i:i+j]).items()) # O(N) time key extract
+            buckets[key] = buckets.get(key, 0) + 1
+    count = 0
+    for key in buckets:
+        count += buckets[key] * (buckets[key]-1) // 2
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = sherlockAndAnagrams(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
